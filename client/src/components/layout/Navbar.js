44ab838a280514ead488fee674ad24b00783b73e
Andrei Togan <togandrei@gmail.com>
2022-05-04 18:53:38 +0300

    Resolve some errors

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,36 +1,42 @@
-import React from 'react';
-import { AiFillHome, AiOutlineLogin } from 'react-icons/ai';
-import { MdOutlineAccountBox } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { AiFillHome, AiOutlineLogin } from "react-icons/ai";
+import { MdOutlineAccountBox } from "react-icons/md";
+import { Link } from "react-router-dom";
+// import logo from ".../images/logo.png";
 
 const Navbar = () => {
-	return (
-		<div>
-			<nav className="navbar bg-primary">
-				<h1>
-					<Link to="/index">SociaL in</Link>
-				</h1>
-				<ul>
-					<li>
-						<Link to="/">
-							<AiFillHome /> Home
-						</Link>
-					</li>
-					<li>
-						<Link to="/login">
-							<AiOutlineLogin /> Log in
-						</Link>
-					</li>
-					<li>
-						<Link to="/register">
-							<MdOutlineAccountBox />
-							Sign up
-						</Link>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	);
+  return (
+    <div>
+      <nav className="navbar bg-primary">
+        {/* <h1>
+          <img src={logo}>
+            <Link to="/index"></Link>
+          </img>
+        </h1> */}
+        <h1>
+          <Link to="/index">r3act SS</Link>
+        </h1>
+        <ul>
+          <li>
+            <Link to="/">
+              <AiFillHome /> Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/login">
+              <AiOutlineLogin /> Log in
+            </Link>
+          </li>
+          <li>
+            <Link to="/register">
+              <MdOutlineAccountBox />
+              Sign up
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
